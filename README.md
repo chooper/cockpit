c75fa60e256209a9a7319c2ebf25de272ed87724
Charles Hooper <charles.hooper@dotcloud.com>
2012-09-23 06:10:12 -0400

    Use mainstream ZeroRPC lib and client so other people can actually use this

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Here is how to get started:
 
 1. Download this package and ``cd`` to it
 2. ``cp cockpit/config.js.dist cockpit/config.js``
-3. In one term: ``cd tools; zerorpc-client --server --bind 'tcp://0:1300' mock.MockServer``
-4. In another: ``cd cockpit && sudo npm install && node app.js``
+3. In one term set up the mock stream server: ``cd tools; pip install -r requirements.txt; zerorpc --server --bind 'tcp://0:1300' mock.MockServer``
+4. In another term run cockpit: ``cd cockpit && sudo npm install && node app.js``
 
 
 Sometimes you have to do ``sudo npm install`` twice for some reason, but I haven't gotten to the bottom of why and I haven't noticed any negative effects yet.
--- tools/mock.py
+++ tools/mock.py
@@ -5,32 +5,29 @@ Mock ZeroRPC server
 ===================
 Exposes some really basic and simple calls. Mainly for testing cockpit.
 
-Usage: ``zerorpc-client --server --bind "tcp://0:$PORT" mock.MockServer``
+Usage: ``zerorpc --server --bind "tcp://0:$PORT" mock.MockServer``
 
 Depends
 -------
-* gevent
-* gevent_zerorpc
+* zerorpc <http://zerorpc.dotcloud.com/>
 """
 
 import time
 from random import randint
-import gevent
-from gevent_zerorpc import zerorpc
+import zerorpc
 
 class MockServer(object):
 
-    mock_data_len = 100
+    mock_data_len = 10000
 
     def simple_req(self):
         mock_data = [randint(0,42) for i in xrange(self.mock_data_len)]
         time.sleep(5)   # pretend it takes some time to process the request
-        return self.mock_data
+        return mock_data
 
     @zerorpc.stream
     def simple_req_streaming(self):
         for i in xrange(self.mock_data_len):
-            gevent.sleep(0) # yield so heartbeats work
             yield randint(0,42)
 
 
